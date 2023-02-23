c622627b08b8d3531bf89083ce5bcab2c1feb3e9
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-02-23 10:31:12 +0700

    Add files via upload

---
+++ Pages/Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalHDT.Pages.Models
+{
+    public class Users : PageModel
+    {
+        [Key, Column(Order = 1)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int BookId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+
+
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$")]
+
+        public string Password { get; set; }
+
+        [NotMapped]
+        [Required]
+        [System.ComponentModel.DataAnnotations.Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
