5c8fd901b3de7d26cfef8ad4e4819e953fb779fe
Iantaif <95291823+Iantaif@users.noreply.github.com>
2023-02-23 10:38:59 +0700

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,30 @@
+﻿using FinalHDT.Pages.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinalHDT
+{
+    public class Startup
+    {
+        public IConfiguration configuration { get; }
+        public Startup(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddSession();
+            var connection = configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<DatabaseContext>(options => options.UseSqlServer(connection));
+        }
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+            app.UseSession();
+        }
+    }
+}
